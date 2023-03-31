9794fd405d41093be16055bba11131e5b5dfbc5d
Connor O'Hara <connor@switchboard.xyz>
2023-03-31 12:03:07 +0200

    simplified

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM alpine:3.17.3
+RUN apk add --no-cache bash
+COPY ./gmd_linux /gmd
+COPY gm1 /root/.gm1
+COPY gm2 /root/.gm2
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/bin/bash", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+VALIDATOR_NAME=validator1
+# VALIDATOR_NAME2=validator2
+CHAIN_ID=gm
+KEY_NAME=key1
+KEY_NAME2=key2
+CHAINFLAG="--chain-id ${CHAIN_ID}"
+TOKEN_AMOUNT="10000000000000000000000000stake"
+STAKING_AMOUNT="1000000000stake"
+
+NAMESPACE_ID="27e3b546191d5fbb"
+DA_BLOCK_HEIGHT=7
+
+/gmd start $HOMEFLAG --log_level debug --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://celestia-local:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT --rpc.laddr tcp://0.0.0.0:26657 $P2PFLAG

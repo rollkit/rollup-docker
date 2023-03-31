c580d92254e99d5cccaf0f865f22a19709c88c23
Connor O'Hara <connor@switchboard.xyz>
2023-03-31 16:22:06 +0200

    works

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM alpine:3.17.3
-RUN apk add --no-cache bash
+RUN apk add --no-cache bash curl jq
 COPY ./gmd_linux /gmd
 COPY gm1 /root/.gm1
 COPY gm2 /root/.gm2
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/bin/bash", "/entrypoint.sh"]
+#ENTRYPOINT ["/bin/bash"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ KEY_NAME2=key2
 CHAINFLAG="--chain-id ${CHAIN_ID}"
 TOKEN_AMOUNT="10000000000000000000000000stake"
 STAKING_AMOUNT="1000000000stake"
-
+DA_BLOCK_HEIGHT=$(curl http://celestia-local:26657/block | jq -r '.result.block.header.height')
+echo DA HEIGHT: $DA_BLOCK_HEIGHT
 NAMESPACE_ID="27e3b546191d5fbb"
-DA_BLOCK_HEIGHT=7
 
 /gmd start $HOMEFLAG --log_level debug --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://celestia-local:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT --rpc.laddr tcp://0.0.0.0:26657 $P2PFLAG
